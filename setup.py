df459464e90264522d076ea1f9a7ef401be92871
nakamamichiko <michinakama@gmail.com>
2020-09-18 12:44:53 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+   name="BMINAapp",
+   version="0.1",
+   author="NAKAMA",
+   description="BMI calculation APP",
+   url="http://michinakama.net/",
+   packages=setuptools.find_packages(),
+   classifiers=[
+       "Programming Language :: Python :: 3.6.9",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ]
+)
+
